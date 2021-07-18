8123693407d9a4852e5ab752520ef4d6091c1f76
Devon <devonross50@yahoo.com>
2021-07-17 21:23:23 -0500

    schemas included

---
+++ Develop/server/schemas/index.js
@@ -0,0 +1,6 @@
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
+
+module.exports = { typeDefs, resolvers }
+
+//stopped at resolvers.js define the query and mutation functionality to work with the Mongoose models
