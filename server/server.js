aee9373f2c576ab5ac1d636ce204b51b13ccb5ab
Devon <devonross50@yahoo.com>
2021-07-24 11:25:35 -0500

    updated file structure

--- server/server.js
+++ server/server.js
@@ -48,9 +48,9 @@ async function startServer() {
 }
 startServer();
 
-if(process.env.NODE_ENV === 'production'){
+
 app.use(express.static(path.join(__dirname, '../client/build')))
-}
+
 
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
