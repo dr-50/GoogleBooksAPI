e10845c3668dc6184799c8ac9f4e5aa1515eafbb
Devon <devonross50@yahoo.com>
2021-07-24 12:17:22 -0500

    test

--- server/server.js
+++ server/server.js
@@ -22,9 +22,9 @@ async function startServer() {
 }
 startServer();
 
-
+if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, '../client/build')))
-
+}
 
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
