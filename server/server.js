0a08025f1b077879a5cadbeb072275a5601ab901
Devon <devonross50@yahoo.com>
2021-07-24 12:01:52 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -9,32 +9,6 @@ const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers
-// })
-
-// await server.start();
-
-// server.applyMiddleware({ app })
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(express.static(path.join(__dirname, '../client/build')))
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'))
-// })
-
-
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`🌍 Now listening on localhost:${PORT}`)
-//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
-//   });
-// });
-
 
 let server = null;
 async function startServer() {
@@ -48,9 +22,9 @@ async function startServer() {
 }
 startServer();
 
-
+if (process.env.NODE_ENV === 'production'){
 app.use(express.static(path.join(__dirname, '../client/build')))
-
+}
 
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
